ea64df2ad6777729c56e5e40e292f3f340921997
serhanif59 <aliserhani@numericable.fr>
2022-11-15 11:38:06 +0100

    quete 03

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+// src/Controller/DefaultController.php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'wild-series_index')]
+    public function index(): Response
+    {
+        return $this->render('/index.html.twig', [
+
+            'website' => 'Wild Series',
+
+        ]);
+    }
+}
