c75e2037c9d784d341fd1254cb7b03a9baa322dd
wilder <aliserhani@numericable.frclear>
2022-11-17 16:44:55 +0100

    quete symphony 11-2 factory::fake  que sur episodes

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 
 
 #[Route('/program', name: 'program_')]
@@ -32,10 +33,39 @@ public function show(int $id, ProgramRepository $programRepository):Response
             'No program with id : '.$id.' found in program\'s table.'
         );
     }
-    
+
     return $this->render('program/show.html.twig', [
         'program' => $program,
         'seasons' => $seasons
     ]);
+}
+
+#[Route('/{programId<^[0-9]+$>}/seasons/{seasonId<^[0-9]+$>}', name: 'season_show')]
+public function showSeason(int $programId, int $seasonId, ProgramRepository $programRepository, SeasonRepository $seasonRepository):Response
+{
+    $program = $programRepository->findOneBy(['id' => $programId]);
+    $season = $seasonRepository->findOneBy(['id' => $seasonId]);
+    if (!$program) {
+        throw $this->createNotFoundException(
+            'No program with id : '. $programId .' found in program\'s table.'
+        );
     }
+    if (!$season) {
+        throw $this->createNotFoundException(
+            'No season with id : '. $seasonId .' found in season\'s table.'
+        );
+    }
+    $episodes = $season->getEpisodes();
+    if (!$episodes) {
+        throw $this->createNotFoundException(
+            'No episodes with : '. $season->getNumber() .' found in episode\'s table.'
+        );
+    }
+
+    return $this->render('program/season_show.html.twig', [
+        'program' => $program,
+        'season' => $season,
+        'episodes' => $episodes
+    ]);
+}
 }
